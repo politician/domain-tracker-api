9d02dfb42ed5f8e1d47a28afa401aaf64adf3a66
Romain Barissat <romain@barissat.com>
2019-06-09 02:53:43 +0700

    Update lab for Travis & Make puppeteer compatible with Heroku

--- api/auctions/godaddy.js
+++ api/auctions/godaddy.js
@@ -24,7 +24,12 @@ exports.plugin = {
       },
       handler: function (req, h) {
         return ((async () => {
-          const browser = await puppeteer.launch()
+          const browser = await puppeteer.launch({
+            'args': [
+              '--no-sandbox',
+              '--disable-setuid-sandbox'
+            ]
+          })
           const page = await browser.newPage()
           const retry = (fn) => fn().catch(retry.bind(fn))
 
